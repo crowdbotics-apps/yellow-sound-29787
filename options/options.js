feecbf47bb69ebd9888a28cd7c41b7555345d429
Crowdbotics <team@crowdbotics.com>
2021-08-13 16:05:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "yellow_sound_29787Identifier",
+  url: "https://yellow_sound_29787Identifier.botics.co",
+  api: "https://yellow_sound_29787Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
